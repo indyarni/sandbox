22d1b74fb743be9fa8d0454424b1dba72eb99ef5
gustavgans <software.arnoldfranke@gmail.com>
2012-12-09 21:31:12 +0100

    test

---
+++ PrimfaktorenZerleger/src/de/frankea/primfaktorenzerleger/PrimfaktorenZerlegerTest.java
@@ -0,0 +1,68 @@
+package de.frankea.primfaktorenzerleger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class PrimfaktorenZerlegerTest {
+
+	List<Integer> istErgebnis;
+	List<Integer> sollErgebnis;
+	
+	@Before
+	public void before() {
+		istErgebnis = new ArrayList<Integer>();
+		sollErgebnis = new ArrayList<Integer>();
+	}
+	
+	@Test
+	public void testFirst() {
+		sollErgebnis.add(1);
+		istErgebnis = Primfaktorenzerleger.zerlegeInFaktoren(1);
+		Assert.assertEquals(sollErgebnis, istErgebnis);
+	}
+	
+	@Test
+	public void testSecond() {
+		sollErgebnis.add(1);
+		sollErgebnis.add(2);
+		istErgebnis = Primfaktorenzerleger.zerlegeInFaktoren(2);
+		Assert.assertEquals(sollErgebnis, istErgebnis);
+	}
+	
+	@Test
+	public void testFuenfzehn() {
+		sollErgebnis.add(1);
+		sollErgebnis.add(3);
+		sollErgebnis.add(5);
+		istErgebnis = Primfaktorenzerleger.zerlegeInFaktoren(15);
+		Assert.assertEquals(sollErgebnis, istErgebnis);
+	}
+	
+	@Test
+	public void testZweihundertEinunddreissig() {
+		sollErgebnis.add(1);
+		sollErgebnis.add(3);
+		sollErgebnis.add(7);
+		sollErgebnis.add(11);
+		istErgebnis = Primfaktorenzerleger.zerlegeInFaktoren(231);
+		Assert.assertEquals(sollErgebnis, istErgebnis);
+	}
+	
+	@Test
+	public void testFaktorEinundsiebzig() {
+		sollErgebnis.add(1);
+		sollErgebnis.add(3);
+		sollErgebnis.add(7);
+		sollErgebnis.add(11);
+		sollErgebnis.add(71);
+		istErgebnis = Primfaktorenzerleger.zerlegeInFaktoren(16401);
+		Assert.assertEquals(sollErgebnis, istErgebnis);
+	}
+	
+	
+}
---
+++ PrimfaktorenZerleger/src/de/frankea/primfaktorenzerleger/Primfaktorenzerleger.java
@@ -0,0 +1,71 @@
+package de.frankea.primfaktorenzerleger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Primfaktorenzerleger {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Bitte geben Sie die Zahl ein, die Sie in Primfaktoren zerlegen wollen.");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		Integer ausgangsWert = 0;
+		try {
+			ausgangsWert = Integer.valueOf(reader.readLine());
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+		for(Integer faktor : zerlegeInFaktoren(ausgangsWert)) {
+			System.out.println(faktor);
+		}
+
+	}
+
+	/**
+	 * @param ausgangsWert Der Wert, der in Primfaktoren zerlegt werden soll.
+	 * @return Eine Liste der Primfaktoren.
+	 */
+	public static List<Integer> zerlegeInFaktoren(Integer ausgangsWert) {
+		List<Integer> ergebnis = new ArrayList<Integer>();				
+		int aktuellePrimzahl = 1;
+		
+		while(aktuellePrimzahl <= ausgangsWert) {
+			if(ausgangsWert%aktuellePrimzahl==0) {
+				ergebnis.add(aktuellePrimzahl);
+			}
+			aktuellePrimzahl = ermittleNaechstePrimzahl(aktuellePrimzahl);			
+		}
+		
+		return ergebnis;
+	}
+
+	private static int ermittleNaechstePrimzahl(int aktuellePrimzahl) {		
+		while(true) {				
+			aktuellePrimzahl++;
+			if(istPrimzahl(aktuellePrimzahl)) {
+				break;
+			}
+		}
+		return aktuellePrimzahl;
+	}
+
+	private static boolean istPrimzahl(int aktuellePrimzahl) {
+		boolean istPrimzahl;
+		istPrimzahl = true;
+		int zaehler = 2;
+		while(zaehler<aktuellePrimzahl) {
+			if(aktuellePrimzahl%zaehler==0) {
+				istPrimzahl = false;
+				break;
+			}
+			zaehler++;
+		}
+		return istPrimzahl;
+	}
+
+}
